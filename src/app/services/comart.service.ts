f75b5097ed32d8ac0d4ddc8c457b4c22ee07d643
Dragon <solootronombre@gmail.com>
2017-09-21 21:22:50 +0800

    fixing paths

--- src/app/services/comart.service.ts
+++ src/app/services/comart.service.ts
@@ -6,8 +6,8 @@ import { NeoWikiConstants } from '../app.constants';
 const ComArtList: ComArt[] = [
     { id: 0, name: "NEO on the moon", description: "", author: "Rufio-1408", src: "./assets/images/NEOSPACEMAN4K.jpg"},
     { id: 1, name: "No one can tell you what NEO is, you have to see for yourself", description: "", author: "justin7942163", src: "./assets/images/NEOMA.png"},
-    { id: 2, name: "NEO in Vegas", description: "", author: "superantsman", src: "/assets/images/Neowelldone.png"},
-    { id: 3, name: "Let NEO Guide us to a Bright Future", description: "", author: "Muixix", src: "/assets/images/Letneoguideus.png"}
+    { id: 2, name: "NEO in Vegas", description: "", author: "superantsman", src: "./assets/images/Neowelldone.png"},
+    { id: 3, name: "Let NEO Guide us to a Bright Future", description: "", author: "Muixix", src: "./assets/images/Letneoguideus.png"}
 ];
 
 @Injectable()
